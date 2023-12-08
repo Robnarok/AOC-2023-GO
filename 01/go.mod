57f41798e78358445d89c94288e764eb12a90a40
Robin Kraft <mail@robnarok.de>
2023-12-08 22:53:56 +0100

    feat: initial Commit

---
+++ 01/go.mod
@@ -0,0 +1,3 @@
+module github.com/Robnarok/AOC-2023-GO/01
+
+go 1.20
---
+++ 01/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	helper "command-line-arguments/home/robin/git/AOC-2023-GO/helper/main.go"
+)
+
+func main() {
+	helper.ReadFile()
+}
